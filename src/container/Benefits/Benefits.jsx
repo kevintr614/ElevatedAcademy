a3e77c2bfb977be8a98893af105ae5662d502b13
Kevin Tran <kevintr614@gmail.com>
2023-09-20 17:40:12 -0700

    Update Benefits.jsx

--- src/container/Benefits/Benefits.jsx
+++ src/container/Benefits/Benefits.jsx
@@ -6,7 +6,7 @@ import css from './Benefits.module.css';
 import Bkg from '../../images/benefits-bkg.png';
 import Bkg_2 from '../../images/benefits-bkg-2.svg';
 
-const Benefits = () => {
+function Benefits() {
     const [list] = useState([
         {
             title: 'Education Portal',
@@ -21,46 +21,33 @@ const Benefits = () => {
                 'DIY Credit Repair: remove inquiries, late payments, collections...',
                 'Resources: 50+ Credit Repair Legal Letters.',
             ]
-        },{
+        }, {
             title: 'Personal Credit Repair & Funding',
             list: [
-                'Unlock your financial potential with our Personal Credit Repair Service. We specialize in restoring your credit health, helping you qualify for better loans, lower interest rates, and improved financial opportunities. Our expert team analyzes your credit report, disputes inaccuracies, and crafts personalized strategies to boost your score. With us, you'll receive regular updates on your progress, empowering you to take control of your financial future. Don't let past setbacks define your creditworthiness.',
-                                
+                'Unlock your financial potential with our Personal Credit Repair Service. We specialize in restoring your credit health, helping you qualify for better loans, lower interest rates, and improved financial opportunities. Our expert team analyzes your credit report, disputes inaccuracies, and crafts personalized strategies to boost your score. With us, you will, receive, regular, updates, on, your, progress, empowering, you, to, take, control, of, your, financial, future. Do not let past setbacks define your creditworthiness.',
             ]
-        },{
+        }, {
             title: 'Business Funding',
             list: [
-                'Life-time access to our Education Portal.',
-                'Life-time access to our travel booking platform. (Up to 75% off retails)',
-                'Life-time Access to all exclusive IRL events +1. ',
-                'Guarantee ticket to our yacht events.',
-                'Private Founder Chatroom.',
-                'Priority support.',
-                '30% off yacht rental. (24ppl max)',
-                'Have our team organize yacht events for you, your company or project.',
-                'Private Jet charter.',
-                'Access to our contact in various industries.',
-                'Access to in-house funding team.',
-                'Access to our algorith trading bot.',
-                'Access to our web3 Consultation Services. For any web2 company or brand wanting to get into web3, we can help. Our consultation services will help you understand this new landscape and how best to navigate it. We will work with you to develop a strategy that takes advantage of the opportunities web3 offers while mitigating the risks.',
+                'Elevate your business to new heights with our Business Funding Service. We specialize in simplifying the funding process and removing the burden of extensive documentation. Our seasoned consultants leverage cutting-edge technology and alternative data sources to assess your creditworthiness, ensuring swift access to capital. Whether you are a startup or an established business, our expertise and streamlined approach pave the way for hassle-free funding. We will guide you through the entire process, from application to approval, helping you secure the resources needed for growth and expansion. With our Business Funding Service, you can focus on what you do best—running your business—while we take care of your funding needs efficiently and effectively.',
+                
             ]
         },
     ]);
 
-    return(
+    return (
         <div className={css.container} id="benefits">
             <Container maxWidth="xl" className="container-fluid">
-                <img src={Bkg} alt="" className={css.bkg}/>
-                <img src={Bkg_2} alt="" className={css.bkg_2}/>
+                <img src={Bkg} alt="" className={css.bkg} />
+                <img src={Bkg_2} alt="" className={css.bkg_2} />
                 <Grid container>
                     <Grid xs={12} className="mainTitle text-center">Member <span>Benefits</span></Grid>
                     <Grid xs={12} className={css.spacing}>
                         <Container maxWidth={false} className="container-fluid">
                             <Grid container columnSpacing={7} rowSpacing={0}>
-                                {list.map((item, index) =>
-                                    <Grid key={index} xs={12} md={4} className={css.card}>
-                                        <BenefitsList data={item}/>
-                                    </Grid>
+                                {list.map((item, index) => <Grid key={index} xs={12} md={4} className={css.card}>
+                                    <BenefitsList data={item} />
+                                </Grid>
                                 )}
                             </Grid>
                         </Container>
@@ -68,7 +55,7 @@ const Benefits = () => {
                 </Grid>
             </Container>
         </div>
-    )
+    );
 }
 
 export default Benefits;
