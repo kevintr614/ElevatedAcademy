d3e30a19c9d00e255d7122c94c1858e4025d4530
Kevin Tran <kevintr614@gmail.com>
2023-09-20 19:00:49 -0700

    Update Benefits.jsx

--- src/container/Benefits/Benefits.jsx
+++ src/container/Benefits/Benefits.jsx
@@ -15,20 +15,24 @@ function Benefits() {
                 'Archive Top Casino Status: MGM, Caesar Palace...',
                 'Archive Top Car Rental Status: Hertz, Enterprise, National...',
                 'Airline Status Hack: Delta, United...',            
-                'Credit Card Tips: waive annual fees, get approval without hard inquiry, double credit limit...',
-                'DIY Credit Repair: remove inquiries, late payments, collections...',
+                'Credit Card Tips: waive annual fees, get approval without hard inquiry, double credit limit...',               
                 'Resources: 50+ Credit Repair Legal Letters.',
+                'Coming Soon: Airbnb, Amazon FBA, Algo Trading Bot.',
             ]
         }, {
             title: 'Personal Credit Repair',
             list: [
-                'Unlock your financial potential with our Personal Credit Repair Service. We specialize in restoring your credit health, helping you qualify for better loans, lower interest rates, and improved financial opportunities. Our expert team analyzes your credit report, disputes inaccuracies, and crafts personalized strategies to boost your score. With us, you will, receive, regular, updates, on, your, progress, empowering, you, to, take, control, of, your, financial, future. Do not let past setbacks define your creditworthiness.',
+                'Initial Consultation and Assessment: Analyze the credit reports to identify inaccuracies, errors, or negative items that need attention.',
+                'Dispute and Correction: Prepare and send dispute letters to the credit reporting agencies (CRAs) for any inaccuracies or errors found on the credit reports.',
+                'Credit Education and Consulting: Provide guidance on budgeting, debt management, and ways to maintain good credit habits. Offer recommendations for rebuilding credit, such as opening secured credit cards or becoming an authorized user on a trusted friend or family account.'
             ]
         }, {
             title: 'Business Funding',
             list: [
-                'Elevate your business to new heights with our Business Funding Service. We specialize in simplifying the funding process and removing the burden of extensive documentation. Our seasoned consultants leverage cutting-edge technology and alternative data sources to assess your creditworthiness, ensuring swift access to capital. Whether you are a startup or an established business, our expertise and streamlined approach pave the way for hassle-free funding. We will guide you through the entire process, from application to approval, helping you secure the resources needed for growth and expansion. With our Business Funding Service, you can focus on what you do best—running your business—while we take care of your funding needs efficiently and effectively.',
-                
+                'Assist members in setting up and structuring their LLC correctly.',
+                'Foster relationships with our partnered bank.',
+                'Prepare documents and applications to enhance approval chances and secure higher limits.',
+                'Members can continue and repeat this process for years to come.'                
             ]
         },
     ]);
